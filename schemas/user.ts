72e4ec01e460e19b7270a57d2926e029e2377b1e
Rejoan Ahmed <rejoanahmed>
2022-02-20 14:08:27 +0600

    sanity project setup

---
+++ schemas/user.ts
@@ -0,0 +1,23 @@
+export default{
+  name:'user',
+  title: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'userName',
+      title: 'UserName',
+      type: 'string'
+    },
+    // {
+    //   name: 'password',
+    //   title: 'Password',
+    //   type: 'string'
+    // },
+    {
+      name: 'image',
+      title: 'Image',
+      type:'string'
+    }
+  ]
+
+}
